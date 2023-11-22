94b85611b3f05abc040261f4c62b0dd7fc785a3a
Yannic Bracke <yannic.bracke@bbaw.de>
2023-11-22 12:32:09 +0100

    Clean up test file

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -14,19 +14,6 @@ def read_jsonl(path: str) -> List[Dict[str, Any]]:
     return data
 
 
-def _tok2raw(tokens: List[str], whitespaces: List[bool]):
-    if whitespaces is not None:
-        raw = ""
-        for ws, tok in zip(whitespaces, tokens):
-            sep = " " if ws else ""
-            raw += f"{sep}{tok}"
-    return raw
-
-
-def dummy_modifier(text: str) -> str:
-    return text
-
-
 class BasicsTester(unittest.TestCase):
     def setUp(self):
         self.data = read_jsonl("tests/testdata/testfile_001.jsonl")
@@ -46,22 +33,22 @@ class BasicsTester(unittest.TestCase):
         assert norm_ws_from_raw == self.data[0]["norm_ws"]
 
     def test_tok2raw(self):
-        orig_raw_from_tok = _tok2raw(self.data[0]["orig_tok"], self.data[0]["orig_ws"])
+        orig_raw_from_tok = self.modifier._tok2raw(self.data[0]["orig_tok"], self.data[0]["orig_ws"])
         assert orig_raw_from_tok == self.data[0]["orig"]
 
-        norm_raw_from_tok = _tok2raw(self.data[0]["norm_tok"], self.data[0]["norm_ws"])
+        norm_raw_from_tok = self.modifier._tok2raw(self.data[0]["norm_tok"], self.data[0]["norm_ws"])
         assert norm_raw_from_tok == self.data[0]["norm"]
 
-    def test_extract_spans(self):
-        spans = get_spans(
-            self.data[0]["orig"], self.data[0]["orig_tok"], self.data[0]["orig_ws"]
-        )
-        assert spans == self.data[0]["orig_spans"]
+    # def test_extract_spans(self):
+    #     spans = get_spans(
+    #         self.data[0]["orig"], self.data[0]["orig_tok"], self.data[0]["orig_ws"]
+    #     )
+    #     assert spans == self.data[0]["orig_spans"]
 
-        spans = get_spans(
-            self.data[0]["norm"], self.data[0]["norm_tok"], self.data[0]["norm_ws"]
-        )
-        assert spans == self.data[0]["norm_spans"]
+    #     spans = get_spans(
+    #         self.data[0]["norm"], self.data[0]["norm_tok"], self.data[0]["norm_ws"]
+    #     )
+    #     assert spans == self.data[0]["norm_spans"]
 
     def test_interal_align(self):
         alignment = self.modifier._align(
