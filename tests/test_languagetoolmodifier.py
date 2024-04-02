4fc5b6a52415e770b9c3073c6cdb3e6989077cb9
Yannic Bracke <yannic.bracke@bbaw.de>
2024-04-02 12:30:40 +0200

    Add test_modify_dataset

--- tests/test_languagetoolmodifier.py
+++ tests/test_languagetoolmodifier.py
@@ -44,3 +44,16 @@ class LanguageToolModifierTester(unittest.TestCase):
         }
         result = self.modifier.modify_sample(input_sample)
         assert result == target
+
+    def test_modify_dataset(self) -> None:
+        data_files = ["tests/testdata/jsonl/dtak/varnhagen_rahel01_1834.jsonl"]
+        dataset = datasets.load_dataset("json", data_files=data_files, split="train")
+        dataset_mod = self.modifier.modify_dataset(dataset)
+        assert (
+            dataset[20]["norm"]
+            == "Und wenn auch der volle Reichtum dieses von Geist und Liebe beseelten Gemütes nicht unmittelbar jedem Auge ganz entfaltet lag, so bekennen doch Alle, die auch nur Momente dieses in Wohlwollen und Wahrheitseifer stets erregten Lebens angeschaut, daß sie von dieser Erscheinung einen seltenen und ahndungsvollen Eindruck der eigentümlichsten Kraft und Anmut empfangen haben, der jeder freigebigsten Voraussetzung Raum gibt, und Alle mitfühlend unserer Wehklage beistimmen läßt."
+        )
+        assert (
+            dataset_mod[20]["norm"]
+            == "Und wenn auch der volle Reichtum dieses von Geist und Liebe beseelten Gemütes nicht unmittelbar jedem Auge ganz entfaltet lag, so bekennen doch Alle, die auch nur Momente dieses in Wohlwollen und Wahrheitseifer stets erregten Lebens angeschaut, daß sie von dieser Erscheinung einen seltenen und ahndungsvollen Eindruck der eigentümlichsten Kraft und Anmut empfangen haben, der jeder freigebigsten Voraussetzung Raum gibt, und Alle mitfühlend unserer Wehklage beistimmen lässt."
+        )
