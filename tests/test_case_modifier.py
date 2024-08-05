2fb8d23becd26a7d2aa1590d70376fbbb6891083
Yannic Bracke <yannic.bracke@bbaw.de>
2024-08-05 12:10:15 +0200

    Update test

--- tests/test_case_modifier.py
+++ tests/test_case_modifier.py
@@ -30,11 +30,12 @@ class CaseModifierTester(unittest.TestCase):
         print(batch_mod["norm"])
         print(batch_mod["norm_tok"])
 
-    def test_modify_dataset_batchsize_1(self) -> None:
+    def test_modify_dataset_batchsize_8(self) -> None:
         data_files = ["tests/testdata/jsonl/dtak/varnhagen_rahel01_1834.jsonl"]
+        # data_files = ["tests/testdata/jsonl/dtak/weigel_moralweissheit_1674.jsonl"]
         self.dataset = datasets.load_dataset(
             "json", data_files=data_files, split="train"
         )
-        self.dataset = self.dataset.select(range(2))
-        dataset_mod = self.modifier.modify_dataset(self.dataset, batch_size=1)
+        self.dataset = self.dataset.select(range(20))
+        dataset_mod = self.modifier.modify_dataset(self.dataset, batch_size=8)
         assert dataset_mod is not None
